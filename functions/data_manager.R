17472f42814ca2941c10a756e2e2872cbcb065f7
Jessica Gorzo <galaxycoff@gmail.com>
2020-09-08 16:56:52 -0400

    error fixes, more robust to potential user inputs

--- functions/data_manager.R
+++ functions/data_manager.R
@@ -12,7 +12,7 @@ load_data <- function(directory_name=getwd(), starttime=NULL, endtime=NULL, tags
   DatePattern = '^[[[:digit:]]{4}-[[[:digit:]]{2}-[[[:digit:]]{2}[T, ][[[:digit:]]{2}:[[[:digit:]]{2}:[[[:digit:]]{2}(.[[[:digit:]]{3})?[Z]?'
   time = "UTC"
 
-  dfs <- function(x) {lapply(x, function(i) {
+  dfs <- function(x, cols=NULL) {lapply(x, function(i) {
     df <- tryCatch({
       if (file.size(i) > 0) {
         read.csv(i,as.is=TRUE, na.strings=c("NA", ""))
@@ -28,21 +28,23 @@ load_data <- function(directory_name=getwd(), starttime=NULL, endtime=NULL, tags
         #pre_count = nrow(df)
         #df = df[!is.na(df[,idx]),]
         #df = df[grepl(DatePattern,df[,idx]),]
-        if(any(grepl("T", df[,idx]))) {df[,idx] <- as.POSIXct(df[,idx],format="%Y-%m-%dT%H:%M:%OS",tz = "UTC")
-        } else {df[,idx] <- as.POSIXct(df[,idx], tz = "UTC")}
+        if(any(grepl("T", df[,idx]))) {df[,idx] <- as.POSIXct(df[,idx],format="%Y-%m-%dT%H:%M:%OS",tz = "UTC", optional=TRUE)
+        } else {df[,idx] <- as.POSIXct(df[,idx], tz = "UTC", optional=TRUE)}
         #post_count = nrow(df)
         #delta = pre_count - post_count
         vals <- df[,idx]} else {vals <- c()} 
       return(vals)})
     reformat <- which(!sapply(timecols, is.null))
     df[,time_cols[reformat]] <- timecols[reformat]
+    
+    #if((!is.null(cols) & !all(cols %in% colnames(df))) | !any(time_cols %in% colnames(df))) {df <- NULL}
     return(df)})
   }
   
   df_merge <- function(files, cols=NULL) {
-    df_list <- dfs(files)
+    df_list <- dfs(files, cols)
     remove <- which(!sapply(df_list, is.data.frame))*-1
-    if (length(remove) > 0) {df_list <- dfs_to_merge[remove]} 
+    if (length(remove) > 0) {df_list <- df_list[remove]} 
     if (length(df_list) > 0) {
       df <- rbindlist(df_list)
       df <- as.data.frame(df)
@@ -58,16 +60,16 @@ load_data <- function(directory_name=getwd(), starttime=NULL, endtime=NULL, tags
         }
         } else if("recorded.at" %in% colnames(df)) {
         df <- df[order(df$recorded.at),]
-        if(!is.null(starttime)) {
+        if(!is.null(starttime) & inherits(starttime, "POSIXct")) {
           attr(starttime, "tzone") <- "UTC"
           df <- df[df$recorded.at > starttime,]
         }
-        if(!is.null(endtime)) {
+        if(!is.null(endtime) & inherits(endtime, "POSIXct")) {
           attr(endtime, "tzone") <- "UTC"
           df <- df[df$recorded.at < endtime,]
         }}
       
-      if(!is.null(cols)) {
+      if(!is.null(cols) & all(cols) %in% colnames(df)) {
         df$ID <- apply(df[,cols],1, paste , collapse = "-" )
         df <- df[!duplicated(df$ID),]
         df$ID <- NULL
@@ -76,13 +78,22 @@ load_data <- function(directory_name=getwd(), starttime=NULL, endtime=NULL, tags
   return(df)}
   
   beep_data <- df_merge(beep_files, c("Time", "RadioId", "TagId", "NodeId"))
+  #beep_data$RadioId <- as.integer(beep_data$RadioId)
 
 #what this does differently here is also checks for and removes records with NA times or times that don't fit the format
 #also converts RecordedAt column to POSIXct
   health_data <- df_merge(list.files(directory_name, pattern = health_pattern, full.names = TRUE, recursive = TRUE), c("Time", "RadioId", "NodeId"))
+  #health_data$RadioId <- as.integer(health_data$RadioId)
 
 #this also converts Time to POSIXct, removes records that have NA time or don't fit the format
   gps_data <- df_merge(list.files(directory_name, pattern = gps_pattern, full.names = TRUE, recursive = TRUE))
+  #gps_data$latitude <- as.numeric(gps_data$latitude)
+  #gps_data$longitude <- as.numeric(gps_data$longitude)
+  #gps_data$altitude <- as.numeric(gps_data$altitude)
+  #gps_data$quality <- as.integer(gps_data$quality)
+  #gps_data$mean.lat <- as.numeric(gps_data$mean.lat)
+  #gps_data$mean.lng <- as.numeric(gps_data$mean.lng)
+  #gps_data$n.fixes <- as.integer(gps_data$n.fixes)
   
   if (!is.null(tags) & !is.null(beep_data) & any(tags %in% beep_data$TagId)) {beep_data <- beep_data[beep_data$TagId %in% tags,]}
 return(list(beep_data, health_data, gps_data))}
@@ -90,20 +101,32 @@ return(list(beep_data, health_data, gps_data))}
 load_node_data <- function(infile) {
   files <- list.files(infile, pattern = "beep*", full.names = TRUE, recursive = TRUE)
   Sam3 <- lapply(files, function(x) {
-    df <- read.csv(x,as.is=TRUE, na.strings=c("NA", ""))
+    df <- tryCatch({
+      if (file.size(x) > 0) {
+        read.csv(x,as.is=TRUE, na.strings=c("NA", ""))
+      }}, error = function(err) {
+        # error handler picks up where error was generated
+        print(paste("Read.table didn't work!:  ",err))
+      })
+    #if(!all((c("time", "id", "rssi") %in% colnames(df)))) {df <- NULL}
     return(df)})
-  dfs <- Sam3[!duplicated(Sam3)]
-  dfs <- Map(cbind, dfs, file = files[!duplicated(Sam3)])
-  nodes <- rbindlist(dfs)
+  dflist <- Sam3[!duplicated(Sam3)]
+  files <- files[!duplicated(Sam3)]
+  remove <- which(!sapply(dflist, is.data.frame))*-1
+  if (length(remove) > 0) {
+    dflist <- dflist[remove]
+    files <- files[remove]} 
+  dflist <- Map(cbind, dflist, file = files)
+  nodes <- rbindlist(dflist)
   nodes <- as.data.frame(nodes)
-  nodes$NodeId <- sapply(strsplit(as.character(nodes$file), "[/]"), function(x) {x[[length(x)-1]]})
+  nodes$NodeId <- toupper(sapply(strsplit(as.character(nodes$file), "[/]"), function(x) {x[[length(x)-1]]}))
   time = "UTC"
-  nodes$Time <- as.POSIXct(nodes$time,format="%Y-%m-%dT%H:%M:%SZ",tz = time)
+  nodes$Time <- as.POSIXct(nodes$time,format="%Y-%m-%dT%H:%M:%SZ",tz = time, optional=TRUE)
   #nodes <- nodes[nodes$Time > as.POSIXct("2020-08-20"),]
   nodes <- nodes[order(nodes$Time, nodes$file),]
   nodes$RadioId <- NA
   nodes$TagId <- nodes$id
-  nodes$TagRSSI <- nodes$rssi
+  nodes$TagRSSI <- as.integer(nodes$rssi)
   nodes$Validated <- NA
 return(nodes)}
 
