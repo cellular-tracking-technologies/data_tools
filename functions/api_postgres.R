a18b82936b7012fd0a74abd95a55c22c4c201e25
Jessica Gorzo <galaxycoff@gmail.com>
2021-06-16 16:57:13 -0400

    auto install requisite packages

--- functions/api_postgres.R
+++ functions/api_postgres.R
@@ -1,3 +1,7 @@
+list.of.packages <- c("httr", "DBI", "RPostgres","data.table","tidyverse")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(httr)
 library(DBI)
 library(RPostgres)
