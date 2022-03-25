f528c30294fd7f9ee5aedcabe175fbbff2399ee6
Jessica Gorzo <galaxycoff@gmail.com>
2022-03-25 16:59:37 -0400

    thanks Luke DeGroote for path argument tweak

--- api_run.R
+++ api_run.R
@@ -2,14 +2,13 @@ source("functions/api_postgres.R")
 start <- Sys.time()
 
 ####SETTINGS####
-outpath <- "~/Documents/data/radio_projects/myproject" 
 my_token <- "your token here"
 db_name <- "mydb"
 myproject <- "CTT Project Name" #this is your project name on your CTT account
 conn <- dbConnect(RPostgres::Postgres(), dbname=db_name)
 ################
 
-get_my_data(my_token, outpath, conn, myproject)
+get_my_data(my_token, "~/Documents/data/radio_projects/myproject" , conn, myproject) #the folder path is where you want your downloaded files to go
 
 update_db(conn, outpath, myproject)
 dbDisconnect(conn)
